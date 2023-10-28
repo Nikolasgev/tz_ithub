c8deb919b19494c7945c0870de3bb93c81c88361
Nikolas_dev <fjgn1234@mail.ru>
2023-10-29 02:42:10 +0300

    Добавлен package_info_plus для отображения версии

--- lib/src/screens/settings_screen.dart
+++ lib/src/screens/settings_screen.dart
@@ -1,34 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart' show PackageInfo;
 import 'package:provider/provider.dart';
 import '../utils/theme.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  late PackageInfo _packageInfo;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadAppVersion();
+  }
+
+  Future<void> _loadAppVersion() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    setState(() {
+      _packageInfo = packageInfo;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final themeChanger = Provider.of<ThemeChanger>(context);
-
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Настройки'),
+        title: const Text("Настройки"),
+        elevation: 0,
       ),
-      body: Column(
-        children: [
-          const ListTile(
-            title: Text('Текущая версия: 1.0.0'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              themeChanger.setTheme(
-                themeChanger.getTheme() == Themes.lightTheme
-                    ? Themes.darkTheme
-                    : Themes.lightTheme,
-              );
-            },
-            child: const Text('Сменить тему'),
-          ),
-        ],
+      body: FutureBuilder<PackageInfo>(
+        future: PackageInfo.fromPlatform(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text('Ошибка при загрузке версии: ${snapshot.error}'));
+          }
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ListTile(
+                  title: Text(
+                    "Текущая версия приложения: ${_packageInfo.version}",
+                    style: const TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    themeChanger.setTheme(
+                      themeChanger.getTheme() == Themes.lightTheme
+                          ? Themes.darkTheme
+                          : Themes.lightTheme,
+                    );
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                    child: Text('Сменить тему', style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w400
+                      ),),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
